ec0b222318120784272f6cbc6c3171223cbd41a0
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2023-07-18 14:46:45 +0300

    Added Protocol and types, OOP, still one bug in main instantiating server_data

--- client.py
+++ client.py
@@ -1,18 +1,72 @@
 import socket
 import threading
+from typing import Protocol
 
 HOST = '127.0.0.1'
 PORT = 1234
 
 
+class Client(Protocol):
+    client: socket
+
+    def listen_for_messages_from_server(self, client):
+        ...
+
+    def send_message_to_server(self, client):
+        ...
+
+    def communicate_to_server(self, client):
+        ...
+
+
+class Client:
+    def __init__(self, client: socket):
+        self.client = client
+
+    def listen_for_messages_from_server(self, client):
+        while 1:
+            message = client.recv(2048).decode('utf-8')
+            if message != '':
+                username = message.split(":")[0]
+                content = message.split(":")[1]
+                print(f'[{username}] {content}')
+            else:
+                print("Message received from client is empty.")
+
+    def send_message_to_server(self, client):
+        while 1:
+            message = input("Message: ")
+            if message != '':
+                client.sendall(message.encode())
+            else:
+                print('Message is empty')
+                exit(0)
+
+    def communicate_to_server(self, client):
+        username = input("Enter username: ")
+        if username != '':
+            client.sendall(username.encode())
+        else:
+            print("Username can not be empty.")
+            exit(0)
+
+        threading.Thread(target=self.listen_for_messages_from_server,
+                         args=(client, )).start()
+
+        self.send_message_to_server(client)
+
+
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_package = Client(client)
 
     try:
         client.connect((HOST, PORT))
         print(f"Successfully connected to server {HOST}:{PORT}")
     except:
         print(f"Unable to connect to server {HOST}:{PORT}")
+
+    client_package.communicate_to_server(client)
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,60 +1,98 @@
 import socket
 import threading
-
-HOST = '127.0.0.1'
-PORT = 1234
-LISTENER_LIMIT = 5
-active_clients = []
+from typing import Protocol
+from dataclasses import dataclass
+from typing import List, Optional
 
 
-def listen_for_messages(client, username):
-    while 1:
-        message = client.recv(2048).decode('utf-8')
-        if message != '':
-            final_msg = username + ':' + message
-            send_messages_to_all(final_msg)
-        else:
-            print(f'empty message sent from client {username}')
+@dataclass
+class ActiveClients:
+    username: str
+    client: socket
 
 
-def send_message_to_client(client, message):
-    client.sendall(message.encode())
+@dataclass
+class ServerConfig:
+    server: socket.socket
+    HOST: str
+    PORT: int
+    LISTENER_LIMIT: int
+    active_clients: List[ActiveClients]
 
 
-def send_messages_to_all(message):
-    for user in active_clients:
-        send_message_to_client(user[1], message)
+class ServerMethods(Protocol):
+
+    def listen_for_messages(self, client, username):
+        ...
+
+    def send_message_to_client(self, client, message):
+        ...
+
+    def send_messages_to_all(self, message):
+        ...
+
+    def client_handler(self, client):
+        ...
 
 
-def client_handler(client):
-    while 1:
-        username = client.recv(2048).decode('utf-8')
-        if username != '':
-            active_clients.append((username, client))
-            break
-        else:
-            print('Missing username.')
+class ServerData():
+    HOST = '127.0.0.1'
+    PORT = 1234
+    LISTENER_LIMIT = 5
+    active_clients = []
 
-    threading.Thread(target=listen_for_messages,
-                     args=(client, username, )).start()
+
+class Server(ServerConfig):
+    server_config: ServerConfig = ServerConfig()
+
+    def listen_for_messages(self, client: socket.socket, username: str):
+        while 1:
+            message = client.recv(2048).decode('utf-8')
+            if message != '':
+                final_msg = username + ':' + message
+                self.send_messages_to_all(final_msg)
+            else:
+                print(f'empty message sent from client {username}')
+
+    def send_message_to_client(self, client: socket.socket, message: str):
+        client.sendall(message.encode())
+
+    def send_messages_to_all(self, message):
+        for user in self.active_clients:
+            self.send_message_to_client(user[1], message)
+
+    def client_handler(self, client):
+        while 1:
+            username = client.recv(2048).decode('utf-8')
+            if username != '':
+                self.active_clients.append((username, client))
+                break
+            else:
+                print('Missing username.')
+
+        threading.Thread(target=self.listen_for_messages,
+                         args=(client, username, )).start()
 
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+    server_data = ServerData()
+    server_obj = Server(server_data)
     try:
-        server.bind((HOST, PORT))
-        print(f"Running the server on {HOST}:{PORT}")
+        server.bind((server_obj.HOST, server_obj.PORT))
+        print(f"Running the server on {server_obj.HOST}:{server_obj.PORT}")
     except:
-        print(f"Unable to bind to host {HOST}:{PORT}")
+        print(f"Unable to bind to host {server_obj.HOST}:{server_obj.PORT}")
 
-    server.listen(LISTENER_LIMIT)
+    server.listen(server_obj.LISTENER_LIMIT)
 
     while 1:
         client, address = server.accept()
-        print(f"Succesfully connected to client. {address[0]} {address[1]}")
+        print(
+            f"Succesfully connected to client. {address[0]} {address[1]}")
 
-        threading.Thread(target=client_handler, args=(client, )).start()
+        threading.Thread(target=server_obj.client_handler,
+                         args=(client, )).start()
 
 
 if __name__ == '__main__':
